ffcd41338393d73eac9e54d7f02a7e58f70a76f4
Yuliia <yutadv@gmail.com>
2023-03-16 21:53:56 +0200

    bonus_ok

--- bonus_task_2_Fibonace/Program.cs
+++ bonus_task_2_Fibonace/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Скласти програму яка має поверати n-не число Фібоначчі за допомогою
+//рекурсії (рекурсія в методі має викликатись один раз)
+
+static double Fibonacci(double n, double preprevious = 1, double previous = 1)
+{
+    if (n < 3)
+        return previous;
+
+    return Fibonacci(n - 1, previous, preprevious + previous);
+}
+
+Console.Write($"Введіть число n: ");
+double n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($" {n} число фібоначі =  {Fibonacci(n)}");
 
